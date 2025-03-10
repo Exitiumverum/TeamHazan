e6ee058bd552f0d7b04285945f54a2c4fe53b8f2
Dan Tayari <kisa@tayari.co.il>
2025-03-10 14:33:40 +0200

    fixed bugs

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,6 +1,8 @@
 import { AppHeader } from './cmps/AppHeader.jsx'
 import { Introduction } from './cmps/Introduction.jsx'
 import { CustomersCritique } from './cmps/CustomersCritique.jsx'
+import { BeforeAfter } from './cmps/BeforeAfter.jsx'
+import { HowItWorks } from './cmps/HowItWorks.jsx'
 
 export function RootCmp() {
   return (
@@ -9,6 +11,8 @@ export function RootCmp() {
       <section className='content'>
         <Introduction />
         {/* Add your child components or routes here */}
+        <BeforeAfter />
+        <HowItWorks />
         <CustomersCritique />
       </section>
     </main>
--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,17 +1,27 @@
-import Logo from '../../assets/imgs/Logo.png'
+import Logo from '../assets/imgs/Logo.png'
 
 export function AppHeader() {
+  function HandleWhatsappClick() {
+    window.open('https://wa.me/972522027364', '_blank')
+  }
+
   return (
     <header className='app-header'>
       <div className='left-links-container'>
+        <a onClick={HandleWhatsappClick} className='nav-links' href='#'>
+          מעדיף לדבר
+        </a>
         <a className='nav-links' href='#'>
-          תוצאות
+          פוזינג
         </a>
       </div>
-      <img src={Logo} alt='Logo' />
+      <img className='logo' src={Logo} alt='Logo' />
       <div className='right-links-container'>
         <a className='nav-links' href='#'>
-          ליווי אישי
+          איך זה עובד
+        </a>
+        <a className='nav-links' href='#'>
+          תוצאות
         </a>
       </div>
       {/* <h1>Team Hazan</h1> */}
